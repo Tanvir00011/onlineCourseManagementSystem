3a5f6822eababfcf8bcc257878081f56045a4802
Tanvir00011 <tanvir15-13327@diu.edu.bd>
2023-07-07 01:14:47 +0600

    1. Dashboard design 2. Dashboard report data show from server done

--- app/Http/Controllers/DashBoardController.php
+++ app/Http/Controllers/DashBoardController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Student;
+use App\Models\Teacher;
+use App\Models\Training;
 
 class DashBoardController extends Controller
 {
     public function index()
     {
-        return view('admin.home.index');
+        $teacherCount = Teacher::count();
+        $studentCount = Student::count();
+        $publishedCourseCount = Training::where('status',1)->count();
+        return view('admin.home.index',  ['teacherCount' => $teacherCount, 'studentCount' => $studentCount, 'publishedCourseCount' => $publishedCourseCount]);
     }
 }
