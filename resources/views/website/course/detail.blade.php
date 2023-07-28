454da12d6b6faa5d7ef68751d455e327a3c20310
Tanvir00011 <tanvir15-13327@diu.edu.bd>
2023-07-28 21:33:59 +0600

    contact form can be use for review section

--- resources/views/website/course/detail.blade.php
+++ resources/views/website/course/detail.blade.php
@@ -66,6 +66,28 @@
                             <div>{!! $course->description !!}</div>
                         </div>
                     </div>
+                    <div class="p-5 rounded box-shadow">
+                        <form action="#" class="row">
+                            <div class="col-12">
+                                <h3>Contact Form</h3>
+                            </div>
+                            <div class="col-lg-6">
+                                <input type="text" name="name" id="name" class="form-control" placeholder="Name" required="">
+                            </div>
+                            <div class="col-lg-6">
+                                <input type="email" class="form-control" name="email" id="email" placeholder="Email Address" required="">
+                            </div>
+                            <div class="col-12">
+                                <input type="text" name="subject" id="subject" class="form-control" placeholder="Subject" required="">
+                            </div>
+                            <div class="col-12">
+                                <textarea class="form-control p-2" name="message" id="message" placeholder="Your Message Here..." required="" style="height: 150px;"></textarea>
+                            </div>
+                            <div class="col-12">
+                                <button class="btn btn-primary" type="submit" value="send">Submit Now</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
                 @if (count($course_materials) > 0)
                     <div class="col-lg-4">
@@ -116,6 +138,7 @@
             </div>
         </div>
     </section>
+
     <!-- /blog-single -->
     <script>
         function handleComplete(checkbox) {
